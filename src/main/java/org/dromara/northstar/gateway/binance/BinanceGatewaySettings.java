da4b2c554986c6a6e26e7133b46eb4281a28fa07
李嘉豪 <lijiahao-zhixin@bosssoft.com.cn>
2023-10-19 16:55:05 +0800

    对接币安网关2-格式化代码

--- src/main/java/org/dromara/northstar/gateway/binance/BinanceGatewaySettings.java
+++ src/main/java/org/dromara/northstar/gateway/binance/BinanceGatewaySettings.java
@@ -2,7 +2,7 @@ package org.dromara.northstar.gateway.binance;
 
 import org.dromara.northstar.common.constant.FieldType;
 import org.dromara.northstar.common.model.DynamicParams;
-import org.dromara.northstar.common.model.GatewaySettings;
+import org.dromara.northstar.common.GatewaySettings;
 import org.dromara.northstar.common.model.Setting;
 
 import lombok.Getter;
@@ -10,11 +10,11 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class BinanceGatewaySettings extends DynamicParams implements GatewaySettings{
+public class BinanceGatewaySettings extends DynamicParams implements GatewaySettings {
 
-	@Setting(label="API Key", order=10, type=FieldType.TEXT)
-	private String apiKey;
-	
-	@Setting(label="Secret Key", order=20, type=FieldType.TEXT)
-	private String secretKey;
+    @Setting(label = "API Key", order = 10, type = FieldType.TEXT)
+    private String apiKey;
+
+    @Setting(label = "Secret Key", order = 20, type = FieldType.TEXT)
+    private String secretKey;
 }
--- src/main/java/org/dromara/northstar/gateway/binance/BinanceMarketGatewayAdapter.java
+++ src/main/java/org/dromara/northstar/gateway/binance/BinanceMarketGatewayAdapter.java
@@ -30,107 +30,101 @@ import xyz.redtorch.pb.CoreField.ContractField;
 @Slf4j
 public class BinanceMarketGatewayAdapter implements MarketGateway {
 
-	private UMWebsocketClientImpl client;
+    private UMWebsocketClientImpl client;
 
-	private ScheduledExecutorService scheduleExec = Executors.newScheduledThreadPool(0);
+    private ScheduledExecutorService scheduleExec = Executors.newScheduledThreadPool(0);
 
-	private ScheduledFuture<?> task;
+    private ScheduledFuture<?> task;
 
-	private long lastActiveTime;
+    private long lastActiveTime;
 
-	private FastEventEngine feEngine;
+    private FastEventEngine feEngine;
 
-	private IMarketCenter mktCenter;
+    private IMarketCenter mktCenter;
 
-	private GatewayDescription gd;
+    private GatewayDescription gd;
 
-	protected ConnectionState connState = ConnectionState.DISCONNECTED;
+    protected ConnectionState connState = ConnectionState.DISCONNECTED;
 
-	public BinanceMarketGatewayAdapter(GatewayDescription gd, FastEventEngine feEngine,IMarketCenter mktCenter) {
-		this.feEngine = feEngine;
-		this.gd = gd;
-		this.mktCenter = mktCenter;
-	}
+    public BinanceMarketGatewayAdapter(FastEventEngine feEngine, GatewayDescription gd, IMarketCenter mktCenter) {
+        this.feEngine = feEngine;
+        this.gd = gd;
+        this.mktCenter = mktCenter;
+    }
 
-	@Override
-	public void connect() {
-		connState = ConnectionState.CONNECTED;
-		client = new UMWebsocketClientImpl();
+    @Override
+    public void connect() {
+        connState = ConnectionState.CONNECTED;
+        client = new UMWebsocketClientImpl();
 
-		if(connState == ConnectionState.CONNECTED) {
-			return;
-		}
-		task = scheduleExec.scheduleAtFixedRate(()->{
-			lastActiveTime = System.currentTimeMillis();
-			try {
-				client.continuousKlineStream("btcusdt", "perpetual", "1m", ((event) -> {
-					System.out.println(event);
-				}));
-				for(Map.Entry<String, SimTickGenerator> e: tickGenMap.entrySet()) {
-					CoreField.TickField tick = e.getValue().generateNextTick(LocalDateTime.now());
-					feEngine.emitEvent(NorthstarEventType.TICK, tick);
-					GatewayContract contract = (GatewayContract) mktCenter.getContract(ChannelType.SIM, tick.getUnifiedSymbol());
-					contract.onTick(tick);
-				}
-			} catch (Exception e) {
-				log.error("模拟行情TICK生成异常", e);
-			}
-		}, 500, 500, TimeUnit.MILLISECONDS);
+        if (connState == ConnectionState.CONNECTED) {
+            return;
+        }
+        task = scheduleExec.scheduleAtFixedRate(() -> {
+            lastActiveTime = System.currentTimeMillis();
+            try {
+                client.continuousKlineStream("btcusdt", "perpetual", "1m", ((event) -> {
+                    System.out.println(event);
+                }));
+            } catch (Exception e) {
+                log.error("模拟行情TICK生成异常", e);
+            }
+        }, 500, 500, TimeUnit.MILLISECONDS);
 
-		connState = ConnectionState.CONNECTED;
-		CompletableFuture.runAsync(() -> feEngine.emitEvent(NorthstarEventType.GATEWAY_READY, gd.getGatewayId()),
-				CompletableFuture.delayedExecutor(2, TimeUnit.SECONDS));
+        connState = ConnectionState.CONNECTED;
+        CompletableFuture.runAsync(() -> feEngine.emitEvent(NorthstarEventType.GATEWAY_READY, gd.getGatewayId()),
+                CompletableFuture.delayedExecutor(2, TimeUnit.SECONDS));
 
-	}
+    }
 
-	@Override
-	public void disconnect() {
-		client.closeAllConnections();
-		connState = ConnectionState.DISCONNECTED;
-	}
+    @Override
+    public void disconnect() {
+        client.closeAllConnections();
+        connState = ConnectionState.DISCONNECTED;
+    }
 
-	@Override
-	public boolean getAuthErrorFlag() {
-		return false;
-	}
+    @Override
+    public boolean getAuthErrorFlag() {
+        return false;
+    }
 
-	@Override
-	public boolean subscribe(ContractField contract) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean subscribe(ContractField contract) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public boolean unsubscribe(ContractField contract) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean unsubscribe(ContractField contract) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public boolean isActive() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean isActive() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public ChannelType channelType() {
-		return ChannelType.BIAN;
-	}
+    @Override
+    public ChannelType channelType() {
+        return ChannelType.BIAN;
+    }
 
-	@Override
-	public GatewayDescription gatewayDescription() {
-		gd.setConnectionState(getConnectionState());
-		return gd;
-	}
+    @Override
+    public GatewayDescription gatewayDescription() {
+        gd.setConnectionState(getConnectionState());
+        return gd;
+    }
 
-	@Override
-	public String gatewayId() {
-		return gd.getGatewayId();
-	}
+    @Override
+    public String gatewayId() {
+        return gd.getGatewayId();
+    }
 
-	@Override
-	public ConnectionState getConnectionState() {
-		return connState;
-	}
+    @Override
+    public ConnectionState getConnectionState() {
+        return connState;
+    }
 
 }
